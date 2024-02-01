dc2b2cc115fa5bbcaead8f532362284b006ff9e4
Khairul <Khairulimran6810@gmail.com>
2024-02-01 13:20:02 +0800

    Created Exceptions class, and some service methods.

--- src/main/java/com/example/day24workshopfinalised/Services/OrderServices.java
+++ src/main/java/com/example/day24workshopfinalised/Services/OrderServices.java
@@ -1,12 +1,36 @@
 package com.example.day24workshopfinalised.Services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.day24workshopfinalised.Models.Order;
+import com.example.day24workshopfinalised.Models.OrderDetails;
+import com.example.day24workshopfinalised.Repositories.OrderDetailsRepository;
+import com.example.day24workshopfinalised.Repositories.OrdersRepository;
 
 @Service
 public class OrderServices {
     
-    // Transaction
+    @Autowired
+    private OrdersRepository ordersRepository;
+
+    @Autowired
+    private OrderDetailsRepository orderDetailsRepository;
+
+    // Transaction - 
+    
+
 
     // Find operations
+    public List<OrderDetails> findOrderDetailsByOrderId(Integer orderId) {
+        return orderDetailsRepository.findOrderDetailsByOrderId(orderId);
+    }
+
+    public List<Order> findOrderByOrderId(Integer orderId) {
+        return ordersRepository.findOrderByOrderId(orderId);
+    }
+
 
 }
